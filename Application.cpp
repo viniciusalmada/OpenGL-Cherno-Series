8cfe3faf3092033b1588cf8dfdbb59fef4be5bf0
vinicius-devel-arch <viniciusalmada10@gmail.com>
2020-04-10 01:26:40 -0300

    Added: shader file
    main.cpp -> Application.cpp

--- Application.cpp
+++ Application.cpp
@@ -85,17 +85,16 @@ int main() {
 	std::string vertexShader = R"glsl(
 		#version 450 core
 		layout(location = 0) in vec4 position;
-		void main(){
-		gl_Position = position;
+		void main() {
+			gl_Position = position;
 		}
 	)glsl";
 
 	std::string fragmentShader = R"glsl(
 		#version 450 core
 		layout(location = 0) out vec4 color;
-		void main()
-		{
-		color = vec4(1.0, 0.0, 0.0, 1.0);
+		void main() {
+			color = vec4(1.0, 0.0, 0.0, 1.0);
 		}
 	)glsl";
 	unsigned int shader = CreateShader(vertexShader, fragmentShader);
